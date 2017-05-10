7ae06712b572206d16117174ddd480558cc712b6
rishiloyola <rishiloyola98245@gmail.com>
2017-05-10 22:23:50 +0530

    Add TFC methods

--- test/request_test.go
+++ test/request_test.go
@@ -3,38 +3,99 @@ package main
 import (
 	"net/http"
 	"testing"
-  "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/vkuznet/transfer2go/core"
+	"github.com/vkuznet/transfer2go/utils"
 )
 
 var url = "http://localhost:8989"
 
-func TestGetReposHandler(t *testing.T) {
+type tests struct {
+	description        string
+	url                string
+	expectedStatusCode int
+	expectedBody       string
+	result						 bool
+}
+
+// Function names are according to api endpoints
+
+func TestStatus(t *testing.T) {
 	assert := assert.New(t)
 
-	tests := []struct {
-		description        string
-		url                string
-		expectedStatusCode int
-		expectedBody       string
-	}{
-		{
-			description:        "missing argument user",
+	test := tests {
+			description:        "Check status of server",
 			url:                url + "/status",
 			expectedStatusCode: 200,
-			expectedBody: "{\"url\":\"http://localhost:8989\",\"name\":\"Test\",\"ts\":1494326276,\"catalog\":\"sqlite3\",\"protocol\":\"cp\",\"backend\":\"/tmp/vk\",\"tool\":\"/bin/cp\",\"toolopts\":\"\",\"agents\":{\"Test\":\"http://localhost:8989\"},\"addrs\":[\"fe80::1\",\"fe80::c2ce:cdff:feea:5421\",\"192.168.1.40\",\"fe80::3425:6dff:fe98:2642\"],\"metrics\":{\"bytes\":0,\"failed\":0,\"in\":0,\"total\":0,\"totalBytes\":0}}",
-    },
+			expectedBody: "Test",
 	}
 
-	for _, tc := range tests {
+	var data map[string]interface{}
 
-		resp, err := http.Get(tc.url)
-    actual, err := ioutil.ReadAll(resp.Body)
+	resp, err := http.Get(test.url)
+	actual, err := ioutil.ReadAll(resp.Body)
 
-		assert.NoError(err)
+	defer resp.Body.Close()
+	json.Unmarshal([]byte(actual), &data)
+	assert.NoError(err)
 
-		assert.Equal(tc.expectedStatusCode, resp.StatusCode, tc.description)
-		assert.Equal(tc.expectedBody, string(actual), tc.description)
+	assert.Equal(test.expectedStatusCode, resp.StatusCode, test.description)
+	assert.Equal(test.expectedBody, data["name"], test.description)
+
+}
+
+func TestAgents(t *testing.T) {
+	assert := assert.New(t)
+
+	test := tests {
+			description:        "Test the list of connected agents",
+			url:                url + "/agents",
+			expectedStatusCode: 200,
+			expectedBody: "http://localhost:8989",
 	}
+
+	var data map[string]interface{}
+
+	resp, err := http.Get(test.url)
+	actual, err := ioutil.ReadAll(resp.Body)
+
+	defer resp.Body.Close()
+	json.Unmarshal([]byte(actual), &data)
+	assert.NoError(err)
+
+	assert.Equal(test.expectedStatusCode, resp.StatusCode, test.description)
+	assert.Equal(test.expectedBody, data["Test"], test.description)
+
+}
+
+
+func TestTFC(t *testing.T) {
+	assert := assert.New(t)
+
+	test := tests {
+			description:        "Check TFC upload functionality",
+			url:                url + "/tfc",
+			expectedStatusCode: 200,
+			expectedBody: "http://localhost:8989",
+	}
+
+	fname := "data/records.json"
+	c, err := ioutil.ReadFile(fname)
+	assert.NoError(err)
+
+	var records []core.CatalogEntry
+	err = json.Unmarshal([]byte(c), &records)
+	assert.NoError(err)
+
+	d, err := json.Marshal(records)
+	assert.NoError(err)
+
+	des := url + "/tfc"
+	resp := utils.FetchResponse(des, d)
+
+	assert.Equal(test.expectedStatusCode, resp.StatusCode, test.description)
+
 }
