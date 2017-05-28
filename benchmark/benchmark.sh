19907b92624b5bf4701ddebe2dc83d2d5165fae7
rishiloyola <rishiloyola98245@gmail.com>
2017-05-28 21:49:33 +0530

    Add bechmark queries

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,4 +4,10 @@ echo "Time taken to insert the data"
 time sqlite3 test.db < data.sql
 
 echo "Time taken to query"
-time sqlite3 test.db "select * from datasets where id=327;" >/dev/null 2>&1
+time sqlite3 test.db "select * from datasets where id=59;" >/dev/null 2>&1 \
+&& sqlite3 test.db "select * from blocks where datasetid=59;" >/dev/null 2>&1 \
+&& sqlite3 test.db "select * from blocks where datasetid=59 AND id=59066;" >/dev/null 2>&1 \
+&& sqlite3 test.db "select * from blocks where datasetid=59 AND id=59033;" >/dev/null 2>&1 \
+&& sqlite3 test.db "select * from files where datasetid=59 AND id=59033 AND id=59033004;" >/dev/null 2>&1 \
+&& sqlite3 test.db "select * from files where datasetid=59 AND id=59033 AND id=59033019;" >/dev/null 2>&1 \
+&& sqlite3 test.db "select * from files where lfn=\"/1GN/XBy/iFl-/1GN/XBy/iFl#XCAo-y6F4v.root\";" >/dev/null 2>&1
