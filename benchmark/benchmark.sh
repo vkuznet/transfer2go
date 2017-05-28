fde34ea5305701ff97934277dc0195340461973f
rishiloyola <rishiloyola98245@gmail.com>
2017-05-28 18:13:21 +0530

    Code refactor

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,7 +2,6 @@
 
 echo "Time taken to insert the data"
 time sqlite3 test.db < data.sql
-end=`(date +%s%N)`
 
 echo "Time taken to query"
 time sqlite3 test.db "select * from datasets where id=327;" >/dev/null 2>&1
