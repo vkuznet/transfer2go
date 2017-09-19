5f907bc10f195109a406cf9d35ba8ba9fec944ca
Valentin Kuznetsov <vkuznet@gmail.com>
2017-09-19 15:27:42 -0400

    Apply gofmt -s

--- test/compare_test.go
+++ test/compare_test.go
@@ -10,26 +10,26 @@ import (
 
 // Compare 1k/10k files of source with the same 1k files of destination
 func TestCompare(t *testing.T) {
-  input := [2]int{1000, 10000}
-  for _, val := range input {
-  	var sourceCatalog []core.CatalogEntry
-  	destinationCatalog := make([]core.CatalogEntry, val)
-  	dataset := "/a/b/c"
-  	block := "/a/b/c#123"
-  	for i := 0; i < val; i++ {
-  		rec := core.CatalogEntry{Dataset: dataset, Block: block, Lfn: block + "-" + dataset + "file" + strconv.Itoa(i) + ".root"}
-  		sourceCatalog = append(sourceCatalog, rec)
-  	}
-  	copy(destinationCatalog[:], sourceCatalog)
-  	start := time.Now()
+	input := [2]int{1000, 10000}
+	for _, val := range input {
+		var sourceCatalog []core.CatalogEntry
+		destinationCatalog := make([]core.CatalogEntry, val)
+		dataset := "/a/b/c"
+		block := "/a/b/c#123"
+		for i := 0; i < val; i++ {
+			rec := core.CatalogEntry{Dataset: dataset, Block: block, Lfn: block + "-" + dataset + "file" + strconv.Itoa(i) + ".root"}
+			sourceCatalog = append(sourceCatalog, rec)
+		}
+		copy(destinationCatalog[:], sourceCatalog)
+		start := time.Now()
 
-  	records := core.CompareRecords(sourceCatalog, destinationCatalog)
-  	elapsed := time.Since(start)
-  	t.Logf("For %d it took %s", val, elapsed)
-  	if records != nil {
-  		t.Errorf("Incorrect Match for 1k files: %d", len(records))
-  	}
-  }
+		records := core.CompareRecords(sourceCatalog, destinationCatalog)
+		elapsed := time.Since(start)
+		t.Logf("For %d it took %s", val, elapsed)
+		if records != nil {
+			t.Errorf("Incorrect Match for 1k files: %d", len(records))
+		}
+	}
 }
 
 // Compare 1k files of source with the distinct 10k files of destination
@@ -53,12 +53,12 @@ func TestCompareTenThousandUncommon(t *testing.T) {
 
 // Test Request function
 func TestGetDestFiles(t *testing.T) {
-  start := time.Now()
-  rec, err := core.GetDestFiles(core.TransferRequest{SrcUrl:"http://localhost:8000", DstUrl:"http://localhost:9000", Dataset: "/a/b/c"})
-  elapsed := time.Since(start)
+	start := time.Now()
+	rec, err := core.GetDestFiles(core.TransferRequest{SrcUrl: "http://localhost:8000", DstUrl: "http://localhost:9000", Dataset: "/a/b/c"})
+	elapsed := time.Since(start)
 	t.Log("Time took to get destination file", elapsed)
-  if err != nil {
+	if err != nil {
 		t.Log("Error in TestGetDestFiles:", err)
 	}
-  t.Log("Files found on destination:", len(rec))
+	t.Log("Files found on destination:", len(rec))
 }
